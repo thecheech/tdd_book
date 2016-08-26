4e8365d7b17b448f8c6e1648a5024757b98fdaa0
kobykarp <newpasS2>
2016-08-26 17:44:17 +0200

    refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # enter the site
         self.browser.get('http://127.0.0.1:8000')
@@ -28,15 +33,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # enter some text in the textbox
         inputbox.send_keys('Buy peacock feathers\n')
-        inputbox.send_keys('Use peacock feathers to make a fly\n')
 
-        # we hit ENTER
-        #inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('FINISH THE TEST!')
 
