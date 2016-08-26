058667bfababec2d5a71d676d4f0337ccde7a8c0
kobykarp <newpasS2>
2016-08-26 14:34:24 +0200

    bug fix

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
-import lists
+from lists.views import home_page
 
 urlpatterns = [
-    url(r'^$', lists.views.home_page, name='home')
+    url(r'^$', home_page, name='home')
 
     # url(r'^admin/', admin.site.urls),
 ]
