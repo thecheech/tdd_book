2c42b1354aa0c357d5bdbf8ca0ac76c33492a164
kobykarp <newpasS2>
2016-08-26 14:42:50 +0200

    test to see if we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,9 +17,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # we check the title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # other stuff...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # enter some text in the textbox
+        inputbox.send_keys('Buy peacock feathers')
+
+        # we hit ENTER
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('FINISH THE TEST!')
 
     if __name__ == '__main__':
         unittest.main(warnings='ignore')
