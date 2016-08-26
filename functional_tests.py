8a77c5279575055d49e7f2be85fb4c5d361c802a
kobykarp <newpasS2>
2016-08-26 17:14:28 +0200

    we are able to insert one line

--- functional_tests.py
+++ functional_tests.py
@@ -27,17 +27,16 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # enter some text in the textbox
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers\n')
+        inputbox.send_keys('Use peacock feathers to make a fly\n')
 
         # we hit ENTER
-        inputbox.send_keys(Keys.ENTER)
+        #inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('FINISH THE TEST!')
 
