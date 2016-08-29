12811e5960e115bf0a9d80f749a9bef39f525a9c
kobykarp <newpasS2>
2016-08-29 14:07:03 +0200

    redirect the post and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -33,6 +33,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # enter some text in the textbox
         inputbox.send_keys('Buy peacock feathers\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly\n')
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
