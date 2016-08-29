3a9353244eb82045d4a87376aabfc1eee5be3c34
kobykarp <newpasS2>
2016-08-29 15:13:11 +0200

    test for a second user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,10 +34,38 @@ class NewVisitorTest(LiveServerTestCase):
 
         # enter some text in the textbox
         inputbox.send_keys('Buy peacock feathers\n')
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly\n')
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        ## using a new session to make sure no info is coming from cookies
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # another user
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # start a new list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk\n')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # check again (?)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+
+        self.assertIn('Buy milk', page_text)
+
 
         self.fail('FINISH THE TEST!')
