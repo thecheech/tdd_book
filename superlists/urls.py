f6015dcd3bd24e31d7773b2aebda74a5175479e4
kobykarp <newpasS2>
2016-08-29 18:24:37 +0200

    passing UTs

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from lists.views import home_page
+from lists import views
 
 urlpatterns = [
-    url(r'^$', home_page, name='home')
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
 
     # url(r'^admin/', admin.site.urls),
 ]
