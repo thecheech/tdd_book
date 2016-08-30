c51c32419c18e2d019b4d3f642c4928a0a9e5919
kobykarp <newpasS2>
2016-08-30 14:18:10 +0200

    full check of second user PASSING THE FT!

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,15 +33,20 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
         # enter some text in the textbox
-        inputbox.send_keys('Buy peacock feathers\n')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
 
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly\n')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        # the page updates again
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         ## using a new session to make sure no info is coming from cookies
         self.browser.quit()
@@ -66,6 +71,3 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
 
         self.assertIn('Buy milk', page_text)
-
-
-        self.fail('FINISH THE TEST!')
